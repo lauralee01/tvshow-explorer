4a08b4f65672f1477d557b3492ab17d63ca6713d
lauralee01 <lauraoghwono@gmail.com>
2018-10-12 16:41:33 +0100

    forms and inputs

--- src/app.js
+++ src/app.js
@@ -3,49 +3,49 @@ console.log('App.js is running!');
 const app = {
 	title: 'Indecision App',
 	subtitle: 'Put your life in the hands of God',
-	options: ['One', 'Two']
+	options: []
+};
+
+const onFormSubmit = (e) => {
+	e.preventDefault();
+
+	const option = e.target.elements.option.value;
+
+	if (option) {
+		app.options.push(option);
+		e.target.elements.option.value = '';
+		renderOptions();
+	}
+};
+
+const removeAll = () => {
+	app.options = [];
+	renderOptions();
 }
 
-// JSX - Javascript XML 
-const template = (
+
+const appRoot = document.getElementById('app');
+
+const renderOptions = () => {
+	const template = (
 	<div>
 		<h1>{app.title}</h1> 
 		{app.subtitle && <p>{app.subtitle}</p>}
 		<p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-
+		<p>{app.options.length}</p>
+		<button onClick={removeAll}>Remove All</button>
 		<ol>
 			<li>Item one</li>
 			<li>Item two</li>
 		</ol>
+		<form onSubmit={onFormSubmit}>
+			<input type="text" name="option"/>
+			<button>Add Option</button>
+		</form>
 	</div>
 );
 
-let count = 0;
-const addOne = () => {
-	count++;
-	renderCounterApp();
-}
-const minusOne = () => {
-	count--;
-	renderCounterApp();
-}
-const reset = () => {
-	count = 0;
-	renderCounterApp();
+ReactDOM.render(template, appRoot);
 }
 
-const appRoot = document.getElementById('app');
-
-const renderCounterApp = () => {
-	const templateTwo = (
-	<div>
-		<h1>Count: {count}</h1>
-		<button onClick={addOne}>+1</button>
-		<button onClick={minusOne}>-1</button>
-		<button onClick={reset}>reset</button>
-	</div>
-);
-
-ReactDOM.render(templateTwo, appRoot);
-};
-renderCounterApp();
+renderOptions();
