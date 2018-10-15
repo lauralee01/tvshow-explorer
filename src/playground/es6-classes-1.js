3814001e3d2aec5611b228dba09e72474918595c
lauralee01 <lauraoghwono@gmail.com>
2018-10-15 16:52:52 +0100

    working with subclasses

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -11,10 +11,42 @@ class Person {
 	}
 }
 
-const me = new Person('Andrew Mead', 26);
-console.log(me.getGreeting());
-console.log(me.getDescription());
+class Student extends Person {
+	constructor(name, age, major) {
+		super(name, age);
+		this.major = major;
+	}
+	hasMajor() {
+		return !!this.major;
+	}
+	getDescription() {
+		let description = super.getDescription();
 
-const other = new Person();
+		if (this.hasMajor()) {
+			description += `Their major is ${this.major}.`;
+		}
+		return description;
+	}
+}
+
+class Traveler extends Person {
+	constructor(name, age, homeLocation) {
+		super(name, age);
+		this.homeLocation = homeLocation;
+	}
+	getGreeting() {
+		let greeting = super.getGreeting();
+
+		if(this.homeLocation) {
+			greeting += ` I'm visiting from ${this.homeLocation}.`;
+		}
+		return greeting;
+	}
+}
+
+const me = new Traveler('Andrew Mead', 26, 'Philadelphia');
+console.log(me.getGreeting());
+
+const other = new Traveler(undefined, undefined, 'Nowhere');
 console.log(other.getGreeting());
-console.log(other.getDescription());
+
