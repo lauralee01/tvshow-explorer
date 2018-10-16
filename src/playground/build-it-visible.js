93e53c6fc6c297449f809a4f91c9badaab978ccf
lauralee01 <lauraoghwono@gmail.com>
2018-10-16 23:40:27 +0100

    adding state to visibility app

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,26 +1,35 @@
-let visibility = false;
-
-const toggleVisibility = () => {
-	visibility = !visibility;
-	render();
-}
-
-const render = () => {
-	const app = (
-	<div>
-		<h1>Visibility Toggle</h1>
-		<button onClick={toggleVisibility}>
-			{visibility ? "hide details" : "show details"}
-		</button>
-		{visibility && (
+class VisibilityToggle extends React.Component {
+	constructor(props) {
+		super(props)
+		this.handleToggleVisibility = this.handleToggleVisibility.bind(this)
+		this.state = {
+			visibility: false
+		}
+	}
+	handleToggleVisibility() {
+		this.setState((prevState) => {
+			return {
+				visibility: !prevState.visibility
+			};
+		});
+	}
+	render() {
+		return(
 			<div>
-				<p>Hey. These are some details you can now see!</p>
+				<h1>Visibility Toggle</h1>
+				<button onClick={this.handleToggleVisibility}>
+				{this.state.visibility ? 'Hide details' : 'Show details'}
+				</button>
+				{this.state.visibility && (
+					<div>
+						<p>Hey. These are some details you can now see! </p>
+					</div>
+				)}
 			</div>
-			)}
-	</div>
-)
-const appRoot = document.getElementById('app')
-ReactDOM.render(app, appRoot);
+		);
+	}
 }
 
-render();
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'))
+
+
